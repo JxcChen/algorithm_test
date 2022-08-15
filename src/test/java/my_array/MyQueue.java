80ffda87791a53b0743361ea4ca8072221d892ef
chenjinxuan <360088940@qq.com>
2022-08-15 18:20:11 +0800

    新增自定义栈和队列

---
+++ src/test/java/my_array/MyQueue.java
@@ -0,0 +1,9 @@
+package my_array;
+
+/**
+ * @author: JJJJ
+ * @date:2022/8/15 9:29
+ * @Description: Desc
+ */
+public class MyQueue {
+}
---
+++ src/test/java/my_array/MyStack.java
@@ -0,0 +1,14 @@
+package my_array;
+
+import java.util.Stack;
+
+/**
+ * @author: JJJJ
+ * @date:2022/8/15 9:29
+ * @Description: Desc
+ */
+public class MyStack<E> {
+    /*
+    实现一个栈，底层运用数组算法
+     */
+}
